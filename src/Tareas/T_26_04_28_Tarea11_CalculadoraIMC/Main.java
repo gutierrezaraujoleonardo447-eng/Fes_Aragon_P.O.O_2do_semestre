fd6d88860c2925521ad6f52811276c10d83e7d62
Leonardo Araujo <gutierrezaraujoleonardo447@gmail.com>
2026-04-27 16:24:34 -0600

    Tarea CalculadoraIMC

---
+++ src/Tareas/T_26_04_28_Tarea11_CalculadoraIMC/Main.java
@@ -0,0 +1,7 @@
+package Tareas.T_26_04_28_Tarea11_CalculadoraIMC;
+
+public class Main {
+    public static void main(String[] args) {
+        CalculadoraIMC c1 = new CalculadoraIMC("CalculadoraIMC");
+    }
+}
